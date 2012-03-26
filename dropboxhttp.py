d37aa5b75f8e6b47e9ea0a1d3134eab07b8f9919
Rian Hunter <rian@alum.mit.edu>
2012-03-25 22:31:08 -0700

    omg massive rehaul

--- dropboxhttp.py
+++ dropboxhttp.py
@@ -1,9 +1,10 @@
+import calendar
 import errno
 import logging
 import os
 import pprint
-import sqlite3
 import tempfile
+import time
 import traceback
 
 try:
@@ -19,6 +20,42 @@ import dropbox
 from dropbox.rest import ErrorResponse
 
 logger = logging.getLogger(__name__)
+HTTP_PRECONDITION_FAILED = 412
+HTTP_NOT_MODIFIED = 304
+HTTP_OK = 200
+
+def tz_offset(tz_string):
+    factor = 1 if tz_string[0] == '+' else -1
+    hours = 3600 * int(tz_string[1:3])
+    minutes = 60 * int(tz_string[3:5])
+    return factor * (hours + minutes)
+
+def dropbox_date_to_posix(date_string):
+    fmt_date, tz = date_string.rsplit(' ', 1)
+    ts = calendar.timegm(time.strptime(fmt_date, "%a, %d %b %Y %H:%M:%S"))
+    return ts + tz_offset(tz)
+
+def posix_to_http_date(ts=None):
+    if ts is None:
+        ts = time.time()
+    HTTP_DATE_FORMAT = "%a, %d %b %Y %H:%M:%S GMT"
+    return time.strftime(HTTP_DATE_FORMAT, time.gmtime(ts))
+
+def http_date_to_posix(date_string):
+    # parse date string in three different formats
+    # 1) Sun, 06 Nov 1994 08:49:37 GMT  ; RFC 822, updated by RFC 1123
+    # 2) Sunday, 06-Nov-94 08:49:37 GMT ; RFC 850, obsoleted by RFC 1036
+    # 3) Sun Nov  6 08:49:37 1994       ; ANSI C's asctime() format
+    for fmt in ["%a, %d %b %Y %H:%M:%S GMT",
+                "%A, %d-%b-%y %H:%M:%S GMT",
+                "%a %b %d %H:%M:%S %Y"]:
+        try:
+            _tt = time.strptime(date_string, fmt)
+        except ValueError:
+            continue
+        return calendar.timegm(_tt)
+    else:
+        raise ValueError("Date could not be parsed")
 
 class AppImpl(object):
     def __init__(self, app_dir):
@@ -154,6 +191,10 @@ def make_app(config, impl):
         start_response('304 NOT MODIFIED', [('Content-type', 'text/plain')])
         return ['Not Modified!']
 
+    def precondition_failed_response(environ, start_response):
+        start_response('412 PRECONDITION FAILED', [('Content-type', 'text/plain')])
+        return ['Precondition Failed!']
+
     def app(environ, start_response):
         # checked if we are linked yet
         if not sess.is_linked():
@@ -170,60 +211,120 @@ def make_app(config, impl):
         else:
             return not_found_response(environ, start_response)
 
-        # TODO: take E-Tag from header
-        try:
-            etag = environ['HTTP_IF_NONE_MATCH']
-        except KeyError:
-            kwargs = {}
-            etag = None
+        # get etag
+        MATCH_ANY = object()
+        def get_match(key_name):
+            try:
+                if_none_match = environ[key_name]
+            except KeyError:
+                return None
+            else:
+                if if_none_match.strip() == "*":
+                    return MATCH_ANY
+                else:
+                    return [a.strip() for a in if_none_match.split(',')]
+
+        if_match = get_match('HTTP_IF_MATCH')
+        if_none_match = get_match('HTTP_IF_NONE_MATCH')
+
+        if (if_none_match is not None and
+            if_none_match is not MATCH_ANY and
+            len(if_none_match) == 1 and
+            if_none_match[0][0] == 'd'):
+            kw = {'hash' : if_none_match[1:]}
         else:
-            kwargs = ({'hash' : etag[1:]}
-                      if etag[0] == 'd' else
-                      {})
+            kw = {}
 
         try:
-            md = client.metadata(path, **kwargs)
+            md = client.metadata(path, **kw)
         except Exception, e:
             if isinstance(e, ErrorResponse):
                 if e.status == 304:
                     return not_modified_response(environ, start_response)
-                elif e.status != 404:
+                elif e.status == 404:
+                    pass
+                else:
                     logger.exception("API Error")
             else:
                 logger.exception("API Error")
+
             is_deleted = True
         else:
             is_deleted = md.get('is_deleted')
 
+        try:
+            last_modified_since = environ['HTTP_IF_MODIFIED_SINCE']
+        except KeyError:
+            last_modified_since = None
+        else:
+            last_modified_since = http_date_to_posix(last_modified_since)
+
+
+        def http_cache_logic(current_etag, current_modified_date, if_match, if_none_match, last_modified_since):
+            logger.debug("current_etag: %r", current_etag)
+            logger.debug("current_modified_date: %r", current_modified_date)
+            logger.debug("if_match: %r", if_match)
+            logger.debug("if_none_match: %r", if_none_match)
+            logger.debug("last_modified_since: %r", last_modified_since)
+
+            if if_match is not None and not (if_match is MATCH_ANY or any(e[1:] == md['rev'] for e in if_match)):
+                logger.debug("precondition failed")
+                return HTTP_PRECONDITION_FAILED
+
+            if ((if_none_match is not None and (if_none_match is MATCH_ANY or any(e == current_etag for e in if_none_match)) and
+                 (last_modified_since is None or current_modified_date is None or current_modified_date <= last_modified_since)) or
+                # this logic sucks, this is the case where if_none_match is not specified
+                (if_none_match is None and last_modified_since is not None and current_modified_date is not None and current_modified_date <= last_modified_since)):
+                logger.debug("not modified")
+                return HTTP_NOT_MODIFIED
+
+            logger.debug("return ok")
+            return HTTP_OK
+
         if is_deleted:
             return not_found_response(environ, start_response)
-        elif md['is_dir']:
-            # it's a directory, we have to re-retrieve
-            # but we should check if the file is the same before
-            # sending out directory contents
-            #md = client.metadata(path)
-            start_response('200 OK', [('Content-type', 'text/plain'),
-                                      ('ETag', 'd' + md['hash'])])
-            return [pprint.pformat(md)]
-        elif etag is not None and md.get('rev') == etag[1:]:
+
+        if md['is_dir']:
+            current_etag = '"d%s"' % (md['hash'].encode('utf8'),)
+            current_modified_date = None
+            def toret(environ, start_response):
+                start_response('200 OK', [('Content-type', 'text/plain'),
+                                          ('ETag', current_etag)])
+                return [pprint.pformat(md)]
+        else:
+            current_etag = '"_%s"' % md['rev'].encode('utf8')
+            current_modified_date = dropbox_date_to_posix(md['modified'].encode('utf8'))
+            def toret(environ, start_response):
+                res = client.get_file(path, rev=md['rev'])
+                def gen():
+                    try:
+                        while True:
+                            ret = res.read(block_size)
+                            if not ret:
+                                break
+                            yield ret
+                    finally:
+                        res.close()
+
+                dropbox_date = md['modified'].encode('utf8')
+                last_modified_date = posix_to_http_date(dropbox_date_to_posix(dropbox_date))
+                start_response('200 OK', [('Content-type', md['mime_type'].encode('utf8')),
+                                          ('Cache-Control', 'public, no-cache'),
+                                          ('Content-Length', str(md['bytes'])),
+                                          ('Date', posix_to_http_date()),
+                                          ('ETag', current_etag),
+                                          ('Last-Modified', last_modified_date)])
+                return gen()
+
+        return_code = http_cache_logic(current_etag, current_modified_date,
+                                       if_match, if_none_match, last_modified_since)
+        if return_code == HTTP_PRECONDITION_FAILED:
+            return precondition_failed_response(environ, start_response)
+        elif return_code == HTTP_NOT_MODIFIED:
             return not_modified_response(environ, start_response)
         else:
-            res = client.get_file(path, rev=md.get('rev'))
-            def gen():
-                try:
-                    while True:
-                        ret = res.read(block_size)
-                        if not ret:
-                            break
-                        yield ret
-                finally:
-                    res.close()
+            return toret(environ, start_response)
 
-            start_response('200 OK', [('Content-type', md['mime_type'].encode('utf8')),
-                                      ("Cache-Control", "public, max-age=3600"),
-                                      ('Content-Length', str(md['bytes'])),
-                                      ('ETag', '"f' + md['rev'].encode('utf8') + '"')])
-            return gen()
     return app
 
 def make_caching(impl):
